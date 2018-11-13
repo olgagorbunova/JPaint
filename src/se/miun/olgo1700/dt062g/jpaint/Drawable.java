9f975714169ded23553c6a70e3bb9b33ac945629
Olga Gorbunova <olgo1700@student.miun.se>
2018-11-13 16:33:56 +0000

    create interface for drawable objects

---
+++ src/se/miun/olgo1700/dt062g/jpaint/Drawable.java
@@ -0,0 +1,13 @@
+package se.miun.olgo1700.dt062g.jpaint;
+
+/**
+* An interface for drawable classes.
+*
+* @author  Olga Gorbunova (olgo1700)
+* @version 1.0
+* @since   2018-11-13
+*/
+public interface Drawable {
+	void draw();
+	void draw(java.awt.Graphics g);
+}
