a621fdc9efc7e6bd1a30a36c8c48d0a5cbc190ba
Olga Gorbunova <olgo1700@student.miun.se>
2018-11-13 23:50:48 +0000

    add Rectangle test to main

--- src/se/miun/olgo1700/dt062g/jpaint/Assignment2.java
+++ src/se/miun/olgo1700/dt062g/jpaint/Assignment2.java
@@ -1,13 +1,59 @@
 package se.miun.olgo1700.dt062g.jpaint;
 
+/**
+* <h1>Assignment 2</h1>
+* This application creates different shapes and calls various
+* methods to print circumference, print area and draw the 
+* shapes to the standard output.
+*
+* @author  Olga Gorbunova (olgo1700)
+* @version 1.0
+* @since   2018-11-13
+*/
 public class Assignment2 {
 
 	public static void main(String[] args) {
+		testRectangle();
+	}
+	
+	private static void testRectangle() {
+		// Create two rectangles. Store one as a Rectangle and
+		// one as a Shape. Draw them.
+		Rectangle r1 = new Rectangle(new Point(0, 0),  "#0000ff");  // blue
+		Shape s1 = new Rectangle(2, -5, "red");
+		System.out.println("Drawing two newly created rectangles...");
+		r1.draw();
+		s1.draw();
 		
-		Rectangle rec = new Rectangle(-7, 0, "#000000");
-		rec.addPoint(-2, 77.99);
-		rec.draw();
+		// Add end points to the rectangles and draw them again
+		Point p1 = new Point(10, 10);
+		System.out.println("\nAdding " + p1 + " as end point to the two rectangles...");
+		r1.addPoint(p1);
+		s1.addPoint(p1);
+		r1.draw();
+		s1.draw();
 		
-		System.out.println(rec.getHeight());
+		// Set new end point to rectangle r1 by calling addPoint with a 
+		// new value and then print the new width
+		r1.addPoint(5, 5);
+		double widthR1 = r1.getWidth();
+		System.out.println("\nChanging end point of r1 to (5.0, 5.0)...");
+		System.out.println("The width of r1 is now: " + widthR1); // 5.0
+		
+		// It is not possible to call getWidth/Height on s1 since it is stored 
+		// as an Shape. We first need to cast it to a Rectangle
+		Rectangle r2 = (Rectangle) s1;
+		System.out.println("\nCasting Shape s1 to a Rectangle (r2)...");
+		System.out.println("The height of r2 (s1) is: " + r2.getHeight()); // 15.0
+		
+		// Change end point on s1. Will r2 change as well???
+		System.out.println("\nChanging end point of s1 to (12.0, 0.0)...");
+		s1.addPoint(12, 0);
+		s1.draw();
+		r2.draw();
+		
+		// Print circumference and area of rectangle s1
+		System.out.println("\nThe circumference of s1 is: " + s1.getCircumference()); // 30.0
+		System.out.println("The area of s1 is: " + s1.getArea()); // 50.0
 	}
 }
