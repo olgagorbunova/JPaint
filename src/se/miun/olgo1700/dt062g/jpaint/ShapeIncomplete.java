27c5caea1e6ab9fdb568f79eaac3f58fb2948f97
Olga Gorbunova <olgo1700@student.miun.se>
2018-11-20 19:43:48 +0000

    add comment

--- src/se/miun/olgo1700/dt062g/jpaint/ShapeIncomplete.java
+++ src/se/miun/olgo1700/dt062g/jpaint/ShapeIncomplete.java
@@ -1,7 +1,7 @@
 package se.miun.olgo1700.dt062g.jpaint;
 
 /**
-* Class ShapeIncomplete is a custom exception class that 
+* Class ShapeIncomplete is a custom exception class for geometric shapes.
 *
 * @author  Olga Gorbunova (olgo1700)
 * @version 1.0
