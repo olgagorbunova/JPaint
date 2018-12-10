c7a325396c26dbcdcd38aec9950ba2c500a835b6
Olga Gorbunova <olgo1700@student.miun.se>
2018-12-10 10:57:12 +0000

    add FileHandler class with saveToXML method

---
+++ src/se/miun/olgo1700/dt062g/jpaint/FileHandler.java
@@ -0,0 +1,67 @@
+package se.miun.olgo1700.dt062g.jpaint;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+* Class includes methods that serialize Drawing objects to XML, as well as extract Drawing objects from an XML file.
+*
+* @author  Olga Gorbunova (olgo1700)
+* @version 1.0
+* @since   2018-12-10
+*/
+public class FileHandler {
+	
+	/**
+	 * Creates an XML file that contains information about a Drawing object, and saves file
+	 * under a given name. Creates file name by adding drawing info to the name as follows:
+	 * "drawing name" by "author".xml. 
+	 * @param drawing Drawing object
+	 */
+	public static void saveToXML(Drawing drawing, String fileName) {
+		
+		//prepare file name
+		String fileNameNew;
+		if(fileName.length() > 4) {
+			String nameEnd = fileName.substring(fileName.length() - 4);
+			
+			if(nameEnd.equals(".xml")) {
+				fileNameNew = fileName;
+			}
+			else
+				fileNameNew = fileName + ".xml";	
+		}
+		else {
+			fileNameNew = fileName + ".xml";	
+		}
+		
+		//generate XML
+		try {
+			JAXBContext jc = JAXBContext.newInstance(Drawing.class);
+			Marshaller marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(drawing, new File(fileNameNew));
+			
+		} catch (JAXBException e) {
+			System.err.println("Error while creating XML");
+		}
+	}
+	
+	/**
+	 * Calls reate an XML file that contains information about a Drawing object, and saves file
+	 * under a given name. Controls if a file name ends with ".xml". If not, adds ".xml" to the file name.
+	 * @param drawing Drawing object
+	 */
+	public static void saveToXML(Drawing drawing) {
+		String fileName = drawing.getName() + " by " + drawing.getAuthor();
+		saveToXML(drawing, fileName);
+	}
+	
+	public static void loadFromXML(String fileName) {
+		
+	}
+
+}
