9cb614cf0c3df6eab1eb1e0203c36283307ed807
Olga Gorbunova <olgo1700@student.miun.se>
2018-12-20 20:39:28 +0000

    add new assignment class

---
+++ src/se/miun/olgo1700/dt062g/jpaint/Assignment6.java
@@ -0,0 +1,25 @@
+package se.miun.olgo1700.dt062g.jpaint;
+import javax.swing.SwingUtilities;
+
+/**
+* <h1>Assignment 6</h1>
+* This class is the starting point for the drawing application.
+* It creates our JFrame and makes it visible.
+* 
+*
+* @author  Olga Gorbunova (olog1700)
+* @version 1.0
+* @since   2018-12-19
+*/
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		// Make sure GUI is created on the event dispatching thread
+		// This will be explained in the lesson about threads
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new PaintFrame().setVisible(true);
+			}
+		});		
+	}
+}
