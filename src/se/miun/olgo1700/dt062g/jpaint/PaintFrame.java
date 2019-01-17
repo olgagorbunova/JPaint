d52aed16f1200dfaf2f9ac16e8a601009708d2b1
Olga Gorbunova <olgo1700@student.miun.se>
2019-01-17 09:02:16 +0000

    Use lambda for functional interface implementation

--- src/se/miun/olgo1700/dt062g/jpaint/PaintFrame.java
+++ src/se/miun/olgo1700/dt062g/jpaint/PaintFrame.java
@@ -23,7 +23,7 @@ public class PaintFrame extends JFrame {
 	private Drawing drawing;
 	
 	private final JPanel toolBar;
-	private final DrawingPanel paintPanel;
+	private DrawingPanel paintPanel = null;
 	private final JPanel[] colorBoxes;
 	private final JPanel colorstate;
 	private final JLabel xy;
@@ -33,7 +33,7 @@ public class PaintFrame extends JFrame {
 	
 	private Point point1;
 	private Shape shape;
-	boolean colorSelected = false;
+	boolean colorSelected = false;	
 	int currentColorIdx;
 	int currentShapeIdx = 0;
 	boolean notADot = false;
@@ -53,136 +53,124 @@ public class PaintFrame extends JFrame {
 		JMenu fileMenu = new JMenu("File");
 		JMenuItem newItem = new JMenuItem("New...");
 		fileMenu.add(newItem);
-		newItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				String name, author = null;
-				name = JOptionPane.showInputDialog(PaintFrame.this, "Enter name of the drawing:");
-				if(name != null)
-					author = JOptionPane.showInputDialog(PaintFrame.this, "Enter author of the drawing:");
-				if(author != null) {
-					if(drawing != null)
-						drawing.clear();
-					drawing = new Drawing(name, author);
-					paintPanel.setDrawing(drawing);
-					displayTitle();
-				}
+		newItem.addActionListener((event) -> {
+			String name, author = null;
+			name = JOptionPane.showInputDialog(PaintFrame.this, "Enter name of the drawing:");
+			if(name != null)
+				author = JOptionPane.showInputDialog(PaintFrame.this, "Enter author of the drawing:");
+			if(author != null) {
+				if(drawing != null)
+					drawing.clear();
+				drawing = new Drawing(name, author);
+				paintPanel.setDrawing(drawing);
+				displayTitle();
 			}
 		});
 		
 		JMenuItem saveItem = new JMenuItem("Save As...");
 		fileMenu.add(saveItem);
-		saveItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				if(drawing != null) {
-					String defaultInput;
-					String dName = drawing.getName();
-					String dAuthor = drawing.getAuthor();
-					if(dName.length() != 0 && dAuthor.length() != 0)
-						defaultInput = dName + " by " + dAuthor + ".xml";
-					else if(dName.length() != 0)
-						defaultInput = dName + ".xml";
-					else if(dAuthor.length() != 0) 
-						defaultInput = dAuthor + ".xml";
-					else {
-						defaultInput = ".xml";
-					}
-					String fileName = JOptionPane.showInputDialog(PaintFrame.this, "Save drawing to:", defaultInput);
-					if(fileName != null)
-						FileHandler.saveToXML(drawing, fileName);
+		
+		saveItem.addActionListener((event) -> {
+			if(drawing != null) {
+				String defaultInput;
+				String dName = drawing.getName();
+				String dAuthor = drawing.getAuthor();
+				if(dName.length() != 0 && dAuthor.length() != 0)
+					defaultInput = dName + " by " + dAuthor + ".xml";
+				else if(dName.length() != 0)
+					defaultInput = dName + ".xml";
+				else if(dAuthor.length() != 0) 
+					defaultInput = dAuthor + ".xml";
+				else {
+					defaultInput = ".xml";
 				}
-				else
-					JOptionPane.showMessageDialog(PaintFrame.this, "No drawing to save");
+				String fileName = JOptionPane.showInputDialog(PaintFrame.this, "Save drawing to:", defaultInput);
+				if(fileName != null)
+					FileHandler.saveToXML(drawing, fileName);
 			}
+			else
+				JOptionPane.showMessageDialog(PaintFrame.this, "No drawing to save");
 		});
 		
 		JMenuItem loadItem = new JMenuItem("Load...");
 		fileMenu.add(loadItem);
-		loadItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				String fileName = JOptionPane.showInputDialog(PaintFrame.this, "Load drawing from:");
-				if(fileName != null) {
-					try {
-						Drawing newDrawing = FileHandler.loadFromXML(fileName);
-						if(drawing == null) {
-							drawing = newDrawing;
-							paintPanel.setDrawing(drawing);
-							displayTitle();
-						}
-						else {
-							paintPanel.addDrawing(newDrawing);
-							LinkedList<Shape> newShapes = newDrawing.getShapes();
-							Iterator<Shape> iterator = newShapes.iterator();
-							while(iterator.hasNext()) {
-								drawing.addShape(iterator.next());
-							}
-						}
-						ownShapesCount = 0;
-					} catch (IllegalArgumentException e) {
-						JOptionPane.showMessageDialog(PaintFrame.this, "No file with given name found");
+		
+		loadItem.addActionListener((event) -> {
+			String fileName = JOptionPane.showInputDialog(PaintFrame.this, "Load drawing from:");
+			if(fileName != null) {
+				try {
+					Drawing newDrawing = FileHandler.loadFromXML(fileName);
+					if(drawing == null) {
+						drawing = newDrawing;
+						paintPanel.setDrawing(drawing);
+						displayTitle();
 					}
+					else {
+						paintPanel.addDrawing(newDrawing);
+						LinkedList<Shape> newShapes = newDrawing.getShapes();
+						Iterator<Shape> iterator = newShapes.iterator();
+						while(iterator.hasNext()) {
+							drawing.addShape(iterator.next());
+						}
+					}
+					ownShapesCount = 0;
+				} catch (IllegalArgumentException e) {
+					JOptionPane.showMessageDialog(PaintFrame.this, "No file with given name found");
 				}
 			}
 		});
 		
 		JMenuItem infoItem = new JMenuItem("Info");
 		fileMenu.add(infoItem);
-		infoItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				if(drawing == null)
-					JOptionPane.showMessageDialog(PaintFrame.this, "Currently no drawing");
-				else
-					JOptionPane.showMessageDialog(PaintFrame.this, drawing.getName() + " by " + drawing.getAuthor() + "\nNumber of shapes: " + drawing.getSize() + "\nTotal area: " + drawing.getTotalArea() + "\nTotal circumference: " + drawing.getTotalCircumference());
-			}
+		infoItem.addActionListener((event) -> {
+			if(drawing == null)
+				JOptionPane.showMessageDialog(PaintFrame.this, "Currently no drawing");
+			else
+				JOptionPane.showMessageDialog(PaintFrame.this, drawing.getName() + " by " + drawing.getAuthor() + "\nNumber of shapes: " + drawing.getSize() + "\nTotal area: " + drawing.getTotalArea() + "\nTotal circumference: " + drawing.getTotalCircumference());
 		});
 		
 		fileMenu.addSeparator();
 		JMenuItem exitItem = new JMenuItem("Exit");
-		fileMenu.add(exitItem);
-		exitItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				System.exit(0);
-			}
+		fileMenu.add(exitItem);		
+		exitItem.addActionListener((event) -> {
+			System.exit(0);
 		});
+
 		
 		JMenu editMenu = new JMenu("Edit");
 		JMenuItem undoItem = new JMenuItem("Undo");
 		editMenu.add(undoItem);
-		undoItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				if(ownShapesCount > 0) {
-					LinkedList<Shape> shapes = drawing.getShapes();
-					LinkedList<Shape> shapesAfterUndo = new LinkedList();
-					for(int i=0; i < drawing.getSize()-1; i++) {
-						shapesAfterUndo.add(shapes.get(i));
-					}
-					drawing.setShapes(shapesAfterUndo);
-					paintPanel.setDrawing(drawing);
-					ownShapesCount -= 1;
+		
+		undoItem.addActionListener((event) -> {
+			if(ownShapesCount > 0) {
+				LinkedList<Shape> shapes = drawing.getShapes();
+				LinkedList<Shape> shapesAfterUndo = new LinkedList();
+				for(int i=0; i < drawing.getSize()-1; i++) {
+					shapesAfterUndo.add(shapes.get(i));
 				}
+				drawing.setShapes(shapesAfterUndo);
+				paintPanel.setDrawing(drawing);
+				ownShapesCount -= 1;
 			}
 		});
 		
 		JMenuItem nameItem = new JMenuItem("Name...");
 		editMenu.add(nameItem);
-		nameItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				String name = JOptionPane.showInputDialog(PaintFrame.this, "Enter name of the drawing:");
-				if(name != null) {
-					drawing.setName(name);
-					displayTitle();
-				}
+		nameItem.addActionListener((event) -> {
+			String name = JOptionPane.showInputDialog(PaintFrame.this, "Enter name of the drawing:");
+			if(name != null) {
+				drawing.setName(name);
+				displayTitle();
 			}
 		});
 		
 		JMenuItem authorItem = new JMenuItem("Author...");
 		editMenu.add(authorItem);
-		authorItem.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				String author = JOptionPane.showInputDialog(PaintFrame.this, "Enter author of the drawing:");
-				if(author != null) {
-					drawing.setAuthor(author);
-					displayTitle();
-				}
+		authorItem.addActionListener((event) -> {
+			String author = JOptionPane.showInputDialog(PaintFrame.this, "Enter author of the drawing:");
+			if(author != null) {
+				drawing.setAuthor(author);
+				displayTitle();
 			}
 		});
 		
@@ -210,14 +198,10 @@ public class PaintFrame extends JFrame {
 		}
 
 		JComboBox<String> shapeChooser = new JComboBox<String>(shapes);
-		shapeChooser.addItemListener(
-			new ItemListener() {
-				public void itemStateChanged(ItemEvent event) {
-					if(event.getStateChange() == ItemEvent.SELECTED)
-						currentShapeIdx = shapeChooser.getSelectedIndex();
-				}
-			}	
-		);
+		shapeChooser.addItemListener((event) -> {
+			if(event.getStateChange() == ItemEvent.SELECTED)
+				currentShapeIdx = shapeChooser.getSelectedIndex();
+		});
 		
 		toolBar.add(colorBar, BorderLayout.CENTER);
 		toolBar.add(shapeChooser, BorderLayout.EAST);
